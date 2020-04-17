1d222be948c9d0d7170719a9b33af4f9055565ea
Eric <49057152+McQueen5258@users.noreply.github.com>
2020-04-17 10:17:26 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".talk1").click(function() {
+  let message1 = $(".speech1").val();
+  $(".speech-bubble1").text(message1);
+});
+
+$(".talk2").click(function() {
+    let message2 = $(".speech2").val();
+    $(".speech-bubble2").text(message2);
+  });
